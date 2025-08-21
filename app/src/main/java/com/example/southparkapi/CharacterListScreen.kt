ecd78a244fadd036ef202d939639ba59724bfa84
Eugene <rezak97@mail.ru>
2025-08-22 00:17:26 +0300

    Функция для отображения списка персонажа

---
+++ app/src/main/java/com/example/southparkapi/CharacterListScreen.kt
@@ -0,0 +1,35 @@
+package com.example.southparkapi
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CharacterListScreen(viewModel: CharacterViewModel = viewModel()) {
+    LaunchedEffect(Unit) {
+        viewModel.loadCharacters()
+    }
+
+    LazyColumn {
+        items(viewModel.characters) { character ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = character.data.name ?: "None",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
