364436bb1250ac8c5353e2ad370fabd99d8f0979
Eugene <rezak97@mail.ru>
2025-08-22 23:03:49 +0300

    Обновлены Data Class

--- app/src/main/java/com/example/southparkapi/Character.kt
+++ app/src/main/java/com/example/southparkapi/Character.kt
@@ -1,10 +1,24 @@
 package com.example.southparkapi
 
 data class CharacterResponse(
-    val data: Character
+    val data: Character,
+)
+
+data class CharacterDetailResponse(
+    val data: CharacterDetail,
 )
 
 data class Character(
     val id: Int,
-    val name: String?
+    val name: String?,
+)
+
+data class CharacterDetail(
+    val id: Int,
+    val name: String?,
+    val age: String?,
+    val sex: String,
+    val hairColor: String,
+    val occupation: String,
+    val religion: String,
 )
