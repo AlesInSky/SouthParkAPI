fad0f20ace979b848b0fea127a50c83de1e8b8c5
Eugene <rezak97@mail.ru>
2025-08-22 23:05:25 +0300

    Реализована навигация между экранами

--- app/src/main/java/com/example/southparkapi/MainActivity.kt
+++ app/src/main/java/com/example/southparkapi/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CharacterListScreen()
+            MyNavigationApp()
         }
     }
 }
 
+@Composable
+fun MyNavigationApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "mainScreen") {
+        composable("mainScreen") {
+            CharacterListScreen(navController)
+        }
+        composable("detailScreen/{characterId}",
+            arguments = listOf(navArgument("characterId") {type = NavType.IntType})) {
+            CharacterDetailsScreen()
+        }
+    }
+}
