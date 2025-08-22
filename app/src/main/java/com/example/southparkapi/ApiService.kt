1a2ef669227bdb3554be2d32dcf6e7a39c798d28
Eugene <rezak97@mail.ru>
2025-08-22 23:03:22 +0300

    Получение данных разделено на два действия

--- app/src/main/java/com/example/southparkapi/ApiService.kt
+++ app/src/main/java/com/example/southparkapi/ApiService.kt
@@ -6,4 +6,7 @@ import retrofit2.http.Path
 interface ApiService {
     @GET("characters/{id}")
     suspend fun getCharacters(@Path("id") id: Int): CharacterResponse
+
+    @GET("characters/{id}")
+    suspend fun getCharactersDetails(@Path("id") id: Int): CharacterDetailResponse
 }
